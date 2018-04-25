1d9076fff55e937cb963b649cd87f6510f235119
Unknown <amyqin501@gmail.com>
2018-04-25 11:31:03 -0400

    Updated second floor map

--- Map/script.js
+++ Map/script.js
@@ -1,17 +1,23 @@
 //display room name, type, and availability when hovered
 function availabilityDisplay(elem) {
+	// Room Name
 	var elementID = elem.getAttribute("id");
-	
+	// Room Hallway
 	var roomType = elem.getAttribute("class");
-	document.getElementById("demo").innerHTML = elementID +" | "+ roomType;
+	
+	if (roomType != "Math Hallway" && roomType != "Science Hallway")
+	{
+		var floorNumber = "First floor";
+	}
+	else
+	{
+		floorNumber= "Second floor"
+	}// End of if (roomType != "Math Hallway" && roomType != "Science Hallway")
+		
+	document.getElementById("placeholderParagraph").innerHTML = elementID +" | "+ roomType +" | " + floorNumber;
 	
 }	
 
 function remove(elem){
-	//document.getElementById("demo").innerHTML = " ";
-}
-
-function test(elem){
-	alert("REEEEEEEEEEE");
 	//document.getElementById("demo").innerHTML = " ";
 }
