0d3d7f7d30db285b3b4e41c6c8ecc4818dc57c44
Unknown <efaz.a.shikder@gmail.com>
2018-04-25 11:20:51 -0400

    Organized directory
    put into assets folder, images in their own folder inside that

--- assets/loginScript.js
+++ assets/loginScript.js
@@ -1,9 +1,11 @@
 // Get the modal
-var modal = document.getElementById('id01');
+var modal = document.getElementById('firstScreen');
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+window.onclick = function(event) 
+{
+  if (event.target == modal)
+  {
     modal.style.display = "none";
   }
 }
