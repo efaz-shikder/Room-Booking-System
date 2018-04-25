d324aeb97d25e550803cde0c1980210178298419
Unknown <amyqin501@gmail.com>
2018-04-24 21:05:51 -0400

    Updated Map

--- Map/script.js
+++ Map/script.js
@@ -1,9 +1,17 @@
 //display room name, type, and availability when hovered
 function availabilityDisplay(elem) {
-	elementID = elem.getAttribute("id");
-	document.getElementById("demo").innerHTML = elementID;
+	var elementID = elem.getAttribute("id");
+	
+	var roomType = elem.getAttribute("class");
+	document.getElementById("demo").innerHTML = elementID +" | "+ roomType;
+	
+}	
+
+function remove(elem){
+	//document.getElementById("demo").innerHTML = " ";
 }
 
-function remove(x){
-	document.getElementById("demo").innerHTML = " ";
+function test(elem){
+	alert("REEEEEEEEEEE");
+	//document.getElementById("demo").innerHTML = " ";
 }
