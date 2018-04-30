55138c21877966b16aa74c983e7ad5cace2728e5
prestedj <josh.prested@gmail.com>
2018-04-30 13:17:49 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 
 // Open a new window with ARBIS floor map.
 function openWindow() {
-    window.open("/*Name of page with amy's map here*/","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=400, height=400");
+    window.open("//Name of page with amy's map here.//","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=400, height=400");
 }
