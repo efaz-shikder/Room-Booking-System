cb02d9682b3928bc04d7b37b4b554912b6a4142a
Unknown <amyqin501@gmail.com>
2018-05-08 19:41:47 -0400

    Auto stash before merge of "master" and "origin/master"

--- Map/script.js
+++ Map/script.js
@@ -5,13 +5,92 @@ function availabilityDisplay(elem) {
 	var roomType = elem.getAttribute("class");
 	document.getElementById("demo").innerHTML = elementID +" | "+ roomType;
 	
+	if (roomType != "Math Hallway" && roomType != "Science Hallway")
+	{
+		var floorNumber = "First floor";
+	}
+	else
+	{
+		floorNumber= "Second floor"
+	}// End of if (roomType != "Math Hallway" && roomType != "Science Hallway")
+		
+	document.getElementById("placeholderParagraph").innerHTML = elementID +" | "+ roomType +" | " + floorNumber;
+	
 }	
 
-function remove(elem){
-	//document.getElementById("demo").innerHTML = " ";
-}
+var englishRoom = document.getElementsByClassName("English Hallway");
+var mathRoom = document.getElementsByClassName("Math Hallway");
+var scienceRoom = document.getElementsByClassName("Science Hallway");
+var sRoom = document.getElementsByClassName("S Hallway");
+var cRoom = document.getElementsByClassName("C Hallway");
+var gymRoom = document.getElementsByClassName("Gym Hallway");
+var frenchRoom = document.getElementsByClassName("French Hallway");
+var musicRoom = document.getElementsByClassName("Music Hallway");
+var frontFoyerRoom = document.getElementsByClassName("Front foyer");
 
-function test(elem){
-	alert("REEEEEEEEEEE");
-	//document.getElementById("demo").innerHTML = " ";
+function hallwayHover(elem){
+	var hallwayType = elem.getAttribute("id");
+	if (hallwayType == "C_Hallway")
+	{
+		for (i = 0; i < cRoom.length; i++) 
+		{
+		cRoom[i].style.fill = "#ffee05";
+		}
+	}
+	else if(hallwayType == "S_Hallway")
+	{
+		for (i = 0; i < sRoom.length; i++) 
+		{
+		sRoom[i].style.fill = "#ffee05";
+		}	
+	}
+	else if(hallwayType == "English_Hallway")
+	{
+		for (i = 0; i < englishRoom.length; i++) 
+		{
+		englishRoom[i].style.fill = "#ffee05";
+		}	
+	}
+	else if(hallwayType == "French_Hallway")
+	{
+		for (i = 0; i < frenchRoom.length; i++) 
+		{
+		frenchRoom[i].style.fill = "#ffee05";
+		}	
+	}
+	else if(hallwayType == "Gym_Hallway")
+	{
+		for (i = 0; i < gymRoom.length; i++) 
+		{
+		gymRoom[i].style.fill = "#ffee05";
+		}	
+	}
+	else if(hallwayType == "Front_Foyer")
+	{
+		for (i = 0; i < frontFoyerRoom.length; i++) 
+		{
+		frontFoyerRoom[i].style.fill = "#ffee05";
+		}
+	}
+	else if(hallwayType == "Music_Hallway")
+	{
+		for (i = 0; i < musicRoom.length; i++) 
+		{
+		musicRoom[i].style.fill = "#ffee05";
+		}	
+	}
+	else if(hallwayType == "Math_Hallway")
+	{
+		for (i = 0; i < mathRoom.length; i++) 
+		{
+		mathRoom[i].style.fill = "#ffee05";
+		}	
+	}
+	else if(hallwayType == "Science_Hallway")
+	{
+	for (i = 0; i < scienceRoom.length; i++) 
+		{
+		scienceRoom[i].style.fill = "#ffee05";
+		}	
+	}
 }
