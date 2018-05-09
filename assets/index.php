513b3aeba1737eac0235befcbafaeb060407c4a1
Ashfi <ashfipathan@gmail.com>
2018-05-09 11:18:28 -0400

    Added PHP connections

---
+++ assets/index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>ARBIS</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+
+    <div class="container">
+      <div class="row justify-content-md-center marginBottom">
+        <div class="col">
+          <img class="img-fluid" src="skrrt.png" alt="ARBIS logo" height="140" width="140">
+        </div>
+      </div>
+      <div class="row justify-content-md-center">
+        <div class="col-md-auto">
+          <!--Sign In Button-->
+          <link href='https://fonts.googleapis.com/css?family=Raleway:400,500,300' rel='stylesheet' type='text/css'>
+          <div id="mainButton">
+          	<div class="btn-text" onclick="openForm()">Sign In</div>
+          	<div class="modal">
+          		<div class="close-button" onclick="closeForm()">x</div>
+          		<div class="form-title">Sign In</div>
+          		<div class="input-group">
+          			<input type="text" id="name" onblur="checkInput(this)" />
+          			<label for="name">Username</label>
+          		</div>
+          		<div class="input-group">
+          			<input type="password" id="password" onblur="checkInput(this)" />
+          			<label for="password">Password</label>
+          		</div>
+              <div class="click-group">
+                <div class="col">
+                  <p class="remember">Remember Me</p>
+                </div>
+                <div class="col">
+                  <div class="show">
+                    <input type="checkbox" id="chk">
+                    <label id="showhide" class="label">Show Password</label>
+                  </div>
+                </div>
+              </div>
+          		<div class="form-button" onclick="closeForm()">Login</div>
+              <div class="form-button" onclick="closeForm()">Sign Up</div>
+              <div class="form-forgot">Forgot Password?</div>
+          	</div>
+          </div>
+        </div>
+      </div>
+    <script src="javascript.js"></script>
+  </body>
+
+  <?php
+    include "/php/connect.php";
+
+    // Wrap up and close connection
+    mysqli_close($server);
+  ?>
+</html>
