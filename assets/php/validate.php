e99d8017dbdced0eaba06dba7f174b6e4cb09259
Ashfi <ashfipathan@gmail.com>
2018-05-14 22:28:15 -0400

    Login system implemented
    Updated index.php and created validate.php to check login details.

---
+++ assets/php/validate.php
@@ -0,0 +1,67 @@
+<?php
+
+session_start();
+
+define("ADMIN", 1);
+$username = "default";
+$password = "default";
+
+if (isset($_POST['submit']))
+{
+	include_once("connect.php");
+
+	if("" == trim($_POST['username']))
+	{
+		$username = "default";
+	}
+	else
+	{
+		// Security measures in order to prevent SQL injections
+		$username = strip_tags($_POST['username']);
+		$password = strip_tags($_POST['password']);
+
+		$username = stripslashes($username);
+		$password = stripslashes($password);
+
+		$username = mysqli_real_escape_string($server, $username);
+		$password = mysqli_real_escape_string($server, $password);
+
+		// Worry about password encryption??
+	}
+	
+
+	$sql = "SELECT * FROM teacher WHERE username = '$username'";
+	$query = mysqli_query($server, $sql);
+	$row = mysqli_fetch_array($query);
+
+	$id = $row['teacherID'];
+	$database_password = $row['password'];
+	$accessLevel = $row['accessLevel'];
+
+	if ($password == $database_password) 
+	{
+		if($accessLevel == ADMIN)
+		{
+			$_SESSION['username'] = $username;
+			$_SESSION['id'] = $id;
+			// header("Location: main.php")
+			echo "Successful login as ADMIN";
+		}
+		else
+		{
+			$_SESSION['username'] = $username;
+			$_SESSION['id'] = $id;
+			// header("Location: main.php")
+			echo "Successful login as TEACHER";
+		}
+	}
+	else
+	{
+		echo "Incorrect login details!";
+	}
+}
+
+// Wrap up and close connection
+mysqli_close($server);
+
+?>
--- index.php
+++ index.php
@@ -23,19 +23,23 @@
       </div>
       <div class="row justify-content-md-center">
         <div class="col-md-auto">
+
           <!--Sign In Button-->
           <link href='https://fonts.googleapis.com/css?family=Raleway:400,500,300' rel='stylesheet' type='text/css'>
+          
           <div id="mainButton">
           	<div class="btn-text" onclick="openForm()">Sign In</div>
           	<div class="modal">
           		<div class="close-button" onclick="closeForm()">x</div>
           		<div class="form-title">Sign In</div>
+
+              <form action="assets/php/validate.php" method="post">
           		<div class="input-group">
-          			<input type="text" id="name" onblur="checkInput(this)" />
+          			<input type="text" id="name" onblur="checkInput(this)" name="username" />
           			<label for="name">Username</label>
           		</div>
           		<div class="input-group">
-          			<input type="password" id="password" onblur="checkInput(this)" />
+          			<input type="password" id="password" onblur="checkInput(this)" name="password" />
           			<label for="password">Password</label>
           		</div>
               <div class="click-group">
@@ -49,11 +53,14 @@
                   </div>
                 </div>
               </div>
-          		<div class="form-button" onclick="closeForm()">Login</div>
+          		<div><input class="form-button" onclick="closeForm()" type="submit" name="submit" value="Login"></div>
+            </form>
+            
               <div class="form-button" onclick="closeForm()">Sign Up</div>
               <div class="form-forgot">Forgot Password?</div>
           	</div>
           </div>
+
         </div>
       </div>
     <script src="assets/javascript/javascript.js"></script>
