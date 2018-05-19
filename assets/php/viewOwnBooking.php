3b40446a969fd5c94213de6a047ff2b1fe8ce05a
notAshfi <ashfipathan@gmail.com>
2018-05-18 21:21:20 -0400

    Modified php
    Modified viewBookings and viewOwnBooking

--- assets/php/viewOwnBooking.php
+++ assets/php/viewOwnBooking.php
@@ -4,34 +4,31 @@ session_start();
 
 include_once("connect.php");
 	
-$currentTeacherID = $_SESSION('id');
-$query = "SELECT * FROM Booking WHERE teacherID = '$currentTeacherID' " ; //You don't need a ; like you do in SQLT
-
-$result = mysql_query($query);
+$query = "SELECT * FROM Booking"; //You don't need a ; like you do in SQL
+$result = mysqli_query($server, $query);
 
 echo "<table>"; // start a table tag in the HTML
 
-while($row = mysql_fetch_array($result))
+$teacherID = 1;
+
+while($row = mysqli_fetch_array($result))
 {   //Creates a loop to loop through results
 
-	$teacherID = $row['teacherID'];
+	$sql1 = "SELECT Booking.dateOfBooking, Booking.period, Booking.classID FROM Booking WHERE Booking.teacherID=$teacherID";
+	$result1 = mysqli_query($server, $sql1);
+	$row1 = mysqli_fetch_array($result1);
+	$classID = $row1['classID'];
 
-	$classID = $row['classID'];
 
-	$sql1 = "SELECT $teacherID FROM 'Teacher' ";
-	$result1 = mysql_query($sql1);
-
-	$firstName = $result1['first_name'];
-	$lastName = $result1['last_name'];
-
-	$sql2 = "SELECT $classID FROM 'Classroom' ";
-	$result2 =  mysql_query($sql2);
-	$roomName = $result2['roomName'];
+	$sql2 = "SELECT Booking.classID, roomName, Classroom.classID FROM Booking INNER JOIN Classroom ON Booking.classID=Classroom.classID WHERE Booking.classID=$classID";
+	$resultRoom =  mysqli_query($server, $sql2);
+	$rowRoom = mysqli_fetch_array($resultRoom);
+	$roomName = $rowRoom['roomName'];
 
 
 
 
-echo "<tr><td>" . $firstName . "</td><td>" . $lastName . "</td><td>" . $roomName . "</td><td>" . $row['dateOfBooking'] . "</td>
+echo "<tr><td>" . $roomName . "</td><td>" . $row['dateOfBooking'] . "</td>
 	<td>" . $row['period'] . "</td></tr>";  //$row['index'] the index here is a field name
 }
 
