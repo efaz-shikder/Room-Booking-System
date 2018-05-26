63278d7ee399ce5eb056a9abf88b349799d3c57a
Unknown <amyqin501@gmail.com>
2018-05-25 20:01:12 -0400

    Updated UI
    -Added theme options

--- homepage/script.js
+++ homepage/script.js
@@ -3,15 +3,15 @@ function availabilityDisplay(elem) {
 	var elementID = elem.getAttribute("id");
 	var roomType = elem.getAttribute("class");
 
-	if (roomType != "Math Hallway" && roomType != "Science Hallway")
+	if (roomType != "Math Hallway" && roomType != "Science Hallway" && roomType !="Geography Hallway")
 	{
 		var floorNumber = "First floor";
 	}
-	else if(roomType = "Geography Hallway")
+	else if(roomType == "Geography Hallway")
 	{
 		floorNumber = "Third floor";
 	}
-	else
+	else if(roomType == "Math Hallway" || roomType == "Science Hallway")
 	{
 		floorNumber= "Second floor"
 	}// End of if (roomType != "Math Hallway" && roomType != "Science Hallway")
@@ -38,71 +38,99 @@ function hallwayHover(elem){
 	{
 		for (i = 0; i < cRoom.length; i++)
 		{
-		cRoom[i].style.fill = "#FFC107";
+		cRoom[i].style.fill = "#6dc5d6";
+		document.getElementById("placeholderParagraph").innerHTML = "C Hallway | First Floor ";
 		}
 	}
 	else if(hallwayType == "S_Hallway")
 	{
 		for (i = 0; i < sRoom.length; i++)
 		{
-		sRoom[i].style.fill = "#FFC107";
+		sRoom[i].style.fill = "#6dc5d6";
+		document.getElementById("placeholderParagraph").innerHTML = "S Hallway | First Floor ";
 		}
 	}
 	else if(hallwayType == "English_Hallway")
 	{
 		for (i = 0; i < englishRoom.length; i++)
 		{
-		englishRoom[i].style.fill = "#FFC107";
+		englishRoom[i].style.fill = "#6dc5d6";
+		document.getElementById("placeholderParagraph").innerHTML = "English Hallway | First Floor ";
 		}
 	}
 	else if(hallwayType == "French_Hallway")
 	{
 		for (i = 0; i < frenchRoom.length; i++)
 		{
-		frenchRoom[i].style.fill = "#FFC107";
+		frenchRoom[i].style.fill = "#6dc5d6";
+		document.getElementById("placeholderParagraph").innerHTML = "French Hallway | First Floor ";
 		}
 	}
 	else if(hallwayType == "Gym_Hallway")
 	{
 		for (i = 0; i < gymRoom.length; i++)
 		{
-		gymRoom[i].style.fill = "#FFC107";
+		gymRoom[i].style.fill = "#6dc5d6";
+		document.getElementById("placeholderParagraph").innerHTML = "Gym Hallway | First Floor ";
 		}
 	}
 	else if(hallwayType == "Front_Foyer")
 	{
 		for (i = 0; i < frontFoyerRoom.length; i++)
 		{
-		frontFoyerRoom[i].style.fill = "#FFC107";
+		frontFoyerRoom[i].style.fill = "#6dc5d6";
+		document.getElementById("placeholderParagraph").innerHTML = "Front Foyer | First Floor ";
 		}
 	}
 	else if(hallwayType == "Music_Hallway")
 	{
 		for (i = 0; i < musicRoom.length; i++)
 		{
-		musicRoom[i].style.fill = "#FFC107";
+		musicRoom[i].style.fill = "#6dc5d6";
+		document.getElementById("placeholderParagraph").innerHTML = "Music Hallway | First Floor ";
 		}
 	}
 	else if(hallwayType == "Math_Hallway")
 	{
 		for (i = 0; i < mathRoom.length; i++)
 		{
-		mathRoom[i].style.fill = "#FFC107";
+		mathRoom[i].style.fill = "#6dc5d6";
+		document.getElementById("placeholderParagraph").innerHTML = "Math Hallway | Second Floor ";
+
 		}
 	}
 	else if(hallwayType == "Science_Hallway")
 	{
 	for (i = 0; i < scienceRoom.length; i++)
 		{
-		scienceRoom[i].style.fill = "#FFC107";
+		scienceRoom[i].style.fill = "#6dc5d6";
+		document.getElementById("placeholderParagraph").innerHTML = "Science Hallway | Second Floor ";
 		}
 	}
 	else if(hallwayType =="Geography_Hallway")
 	{
 	for (i = 0; i < geographyRoom.length; i++) 
 		{
-		geographyRoom[i].style.fill = "#FFC107";
+		geographyRoom[i].style.fill = "#6dc5d6";
+		document.getElementById("placeholderParagraph").innerHTML = "Geography Hallway | Third Floor ";
 		}	
+	}
+}
+
+function themeChange(elem)
+{
+	var checkedTheme = elem.getAttribute("value");
+	if(checkedTheme == "black")
+	{
+	document.getElementById("mainBody").setAttribute("background", "websiteBackground1.jpg");
+	}
+	else if(checkedTheme == "brown")
+	{
+	document.getElementById("mainBody").setAttribute("background", "websiteBackground2.jpg");
+	}
+	else if (checkedTheme == "light")
+	{
+	document.getElementById("mainBody").setAttribute("background", "websiteBackground3.jpg");
 	}
 }
 
@@ -112,8 +140,9 @@ function hallwayHoverOut()
 
 	var map = document.getElementsByTagName("path");
 	for (i = 0; i < map.length; i++) {
-	map[i].style.fill = "#818181";
+	map[i].style.fill = "#5e7172";
 	}
+	document.getElementById("placeholderParagraph").innerHTML = "Room name | Hallway | Floor"
 }
 
 function remove(elem)
