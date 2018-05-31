d9b702d9ec3fc824708395e60a190ad56200efa1
Unknown <ashfipathan@gmail.com>
2018-05-31 00:55:13 -0400

    Remove Classroom Script

--- assets/php/removeClassroom.php
+++ assets/php/removeClassroom.php
@@ -1,5 +1,19 @@
 <?php
+	include_once("connect.php");
 
-	// Removing classroom will remove the ID and will affect old bookings --> cannot delete classrooms 
+	$sql1 = "SET FOREIGN_KEY_CHECKS=0";
+	mysqli_query($server, $sql1);
+
+	$sql2 = "TRUNCATE classroom";
+	mysqli_query($server, $sql2);
+
+	$sql3 = "SET FOREIGN_KEY_CHECKS=1";
+	mysqli_query($server, $sql3);
+
+	echo "All Classrooms have been deleted.";
+
+
+	// Wrap up and close connection
+mysqli_close($server);
 
 ?>
