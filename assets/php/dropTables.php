67f16df22f58a4aeaa4623e5f88629576faac5f7
Unknown <efaz.a.shikder@gmail.com>
2018-05-31 03:59:55 -0400

    drop table not drop

--- assets/php/dropTables.php
+++ assets/php/dropTables.php
@@ -4,16 +4,16 @@
 	$sql1 = "SET FOREIGN_KEY_CHECKS=0";
 	mysqli_query($server, $sql1);
 
-	$sql2 = "DROP booking";
+	$sql2 = "DROP TABLE booking";
 	mysqli_query($server, $sql2);
 	
-	$sql3 = "DROP classroom";
+	$sql3 = "DROP TABLE classroom";
 	mysqli_query($server, $sql3);
 
-	$sql4 = "DROP teacher";
+	$sql4 = "DROP TABLE teacher";
 	mysqli_query($server, $sql4);
 	
-	$sql4 = "DROP schedule";
+	$sql4 = "DROP TABLE schedule";
 	mysqli_query($server, $sql5);
 
 	$sql6 = "SET FOREIGN_KEY_CHECKS=1";
