ae2428f2168d0de192c082952127294adc9a4f7c
Unknown <ashfipathan@gmail.com>
2018-06-03 19:29:39 -0400

    fixed viewOwnBooking bug

--- assets/php/viewOwnBooking.php
+++ assets/php/viewOwnBooking.php
@@ -55,27 +55,27 @@ $teacherEmail = $_SESSION['email'];
 					<th> Teacher</th>
 				</thead>
 				<?php
- 
-              $currentTeacherID = $_SESSION['email'];
-              $query = "SELECT * FROM booking WHERE booking.teacherEmail = '$currentTeacherID' " ;
- 
-              $result = mysqli_query($server, $query);
-              while($row = mysqli_fetch_array($result))
+
+        $currentTeacherID = $_SESSION['email'];
+        $query = "SELECT * FROM booking WHERE booking.teacherEmail = '$currentTeacherID' " ;
+
+        $result = mysqli_query($server, $query);
+        while($row = mysqli_fetch_array($result))
               {   //Creates a loop to loop through results
- 
- 
+
+
                 $dateOfBooking = $row['dateOfBooking'];
                 $period = $row['period'];
                 $classID = $row['classID'];
- 
+
                 $sql = "SELECT * FROM classroom WHERE classroom.classID = $classID";
                 $resultClassroom =  mysqli_query($server, $sql);
                 $resultName = mysqli_fetch_array($resultClassroom);
- 
+
                 $roomName = $resultName['roomName'];
- 
+
                 ?>
- 
+
                 <tbody>
                   <tr id="delete<?php echo $dateOfBooking; echo "$classID"; echo "$period"; ?>">
                     <td><?php echo $dateOfBooking ?></td>
@@ -87,90 +87,49 @@ $teacherEmail = $_SESSION['email'];
                   </tr>
                 </tbody>
               <?php } ?>
-			</table> 
-		</section>
+            </table> 
+          </section>
 
-				$currentTeacherID = $_SESSION['email'];
-				$query = "SELECT * FROM booking" ;
+          <script src="../javascript/jquery.min.js"></script>
+          <script src="../javascript/script.js"></script>
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+          <script type="text/javascript">
+           var action = 1;
 
-				$result = mysqli_query($server, $query);
-				while($row = mysqli_fetch_array($result))
-              {   //Creates a loop to loop through results
+           function toggleNav() {
+            if ( action == 1 ) {
+             document.getElementById("ArbisNav").style.width = "250px";
+             document.getElementById("main").style.marginLeft = "280px";
+             action = 2;
+           }
+           else {
+             document.getElementById("ArbisNav").style.width = "0px";
+             document.getElementById("main").style.marginLeft = "0px";
+             action = 1;
+           }
+           $("#mainContent").toggle();
+         }
+         $(document).ready(function(){
+          $('#nav-icon3').click(function(){
+           $(this).toggleClass('open');
+         });
+        });
 
+      </script>
+      <script type="text/javascript">
+       function deleteAjax(date, room, periods)
+       {
+        var dateOfBooking = JSON.stringify(date);
+        var classID = JSON.stringify(room);
+        var period = JSON.stringify(periods);
 
-              	$dateOfBooking = $row['dateOfBooking'];
-              	$period = $row['period'];
-              	$classID = $row['classID'];
-              	$teacherID = $row['teacherEmail'];
+        if (confirm('Are you sure you want to delete booking')) {
 
-              	$sql1 = "SELECT * FROM classroom WHERE classroom.classID = $classID";
-              	$resultClassroom =  mysqli_query($server, $sql1);
-              	$resultName = mysqli_fetch_array($resultClassroom);
-              	$roomName = $resultName['roomName'];
+         $.ajax({
 
-              	$sql2 = "SELECT * FROM teacher WHERE teacher.email = '$teacherID'";
-              	$resultTeacher = mysqli_query($server, $sql2);
-              	$teacher = mysqli_fetch_array($resultTeacher);
-              	$firstName = $teacher['first_name'];
-              	$lastName = $teacher['last_name'];
-
-              	?>
-
-              	<tbody>
-              		<tr id="delete<?php echo $dateOfBooking; echo "$classID"; echo "$period"; ?>">
-              			<td><?php echo $dateOfBooking ?></td>
-              			<td><?php echo $roomName ?></td>
-              			<td><?php echo $period ?></td>
-              			<td >
-              				<?php echo "$firstName $lastName"; ?>
-              			</td>
-              		</tr>
-              	</tbody>
-              <?php } ?>
-          </table> 
-      </div>
-  </section>
-
-
-  <script src="../javascript/jquery.min.js"></script>
-  <script src="../javascript/script.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script type="text/javascript">
-  	var action = 1;
-
-  	function toggleNav() {
-  		if ( action == 1 ) {
-  			document.getElementById("ArbisNav").style.width = "250px";
-  			document.getElementById("main").style.marginLeft = "280px";
-  			action = 2;
-  		}
-  		else {
-  			document.getElementById("ArbisNav").style.width = "0px";
-  			document.getElementById("main").style.marginLeft = "0px";
-  			action = 1;
-  		}
-  		$("#mainContent").toggle();
-  	}
-  	$(document).ready(function(){
-  		$('#nav-icon3').click(function(){
-  			$(this).toggleClass('open');
-  		});
-  	});
-
-  </script>
-  <script type="text/javascript">
-  	function deleteAjax(date, room, periods)
-  	{
-  		var dateOfBooking = JSON.stringify(date);
-  		var classID = JSON.stringify(room);
-  		var period = JSON.stringify(periods);
-
-  		if (confirm('Are you sure you want to delete booking')) {
-
-  			$.ajax({
-
+          <?php
 // Wrap up and close connection
-mysqli_close($server);
+          mysqli_close($server);
 
 
-?>
+          ?>
