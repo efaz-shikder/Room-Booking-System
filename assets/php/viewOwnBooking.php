4bf750de7d9efc78dbe8e5b1110cc77edf2d0f2b
Unknown <efaz.a.shikder@gmail.com>
2018-06-03 17:26:17 -0400

    deleted unnecessary functions at bottom

--- assets/php/viewOwnBooking.php
+++ assets/php/viewOwnBooking.php
@@ -165,53 +165,6 @@ $teacherEmail = $_SESSION['email'];
 
 	<?php
 
-	function viewOwnBooking()
-	{
-		session_start();
-
-		include_once("connect.php");
-
-
-		$currentTeacherID = $_SESSION['email'];
-		$query = "SELECT * FROM booking WHERE booking.teacherEmail = '$currentTeacherID' " ;
-
-		$result = mysqli_query($server, $query);
-
-		while($row = mysqli_fetch_array($result))
-	{   //Creates a loop to loop through results
-
-
-		$dateOfBooking = $row['dateOfBooking'];
-		$period = $row['period'];
-		$classID = $row['classID'];
-	}
-
-
-	$sql = "SELECT * FROM classroom WHERE classroom.classID = $classID";
-	$resultClassroom =  mysqli_query($server, $sql);
-	$resultName = mysqli_fetch_array($resultClassroom);
-
-	$roomName = $resultName['roomName'];
-
-	$currentDate = date("d.m.Y");
-
-
-
-
-	$cancelEditButton = '<input type="submit" name="submit" class="submit" value="Cancel">
-
-	<input type="submit" value="Edit" onclick="alert('.'You clicked the button!'.')">';
-
-
-	if (strtotime($dateOfBooking) < strtotime($currentDate))
-	{
-		$cancelEditButton = "Unable to Cancel or Edit this Booking.";
-	}
-
-	echo "<tr><td>" . $dateOfBooking . "</td><td>" . $roomName . "</td>
-	<td>" . $row['period'] . "</td><td>" . $cancelEditButton . "</td></tr>";
-}
-
 // Wrap up and close connection
 mysqli_close($server);
 
