bd7b90f47bc8936252d2d12e660b3b3bfbbe185b
Shaibal <shaibalmuhtadee@gmail.com>
2018-06-03 23:16:34 -0400

    added calendars

--- assets/php/ADMIN/viewBookingAdmin.php
+++ assets/php/ADMIN/viewBookingAdmin.php
@@ -48,60 +48,66 @@ $teacherEmail = $_SESSION['email'];
 
 			</div>
 
+			<div class="calendar col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<input type="date" value="yyyy-mm-dd">
+			</div>
+			<div class="row">
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<table id="bookings">
+						<thead>
+							<th>Date</th>
+							<th>Room Number</th>
+							<th>Period</th>
+							<th>Teacher</th>
+							<th>Cancel</th>
+						</thead>
+						<?php
 
-			<table id="bookings">
-				<thead>
-					<th>Date</th>
-					<th>Room Number</th>
-					<th>Period</th>
-					<th>Teacher</th>
-					<th>Cancel</th> 
-				</thead>
-				<?php
- 
-              $currentTeacherID = $_SESSION['email'];
-              $query = "SELECT * FROM booking" ;
- 
-              $result = mysqli_query($server, $query);
-              while($row = mysqli_fetch_array($result))
-              {   //Creates a loop to loop through results
- 
- 
-                $dateOfBooking = $row['dateOfBooking'];
-                $period = $row['period'];
-                $classID = $row['classID'];
-                $teacherID = $row['teacherEmail'];
- 
-                $sql1 = "SELECT * FROM classroom WHERE classroom.classID = $classID";
-                $resultClassroom =  mysqli_query($server, $sql1);
-                $resultName = mysqli_fetch_array($resultClassroom);
-                $roomName = $resultName['roomName'];
+		              $currentTeacherID = $_SESSION['email'];
+		              $query = "SELECT * FROM booking" ;
 
-                $sql2 = "SELECT * FROM teacher WHERE teacher.email = '$teacherID'";
-                $resultTeacher = mysqli_query($server, $sql2);
-                $teacher = mysqli_fetch_array($resultTeacher);
-                $firstName = $teacher['first_name'];
-                $lastName = $teacher['last_name'];
- 
-                ?>
- 
-                <tbody>
-                  <tr id="delete<?php echo $dateOfBooking; echo "$classID"; echo "$period"; ?>">
-                    <td><?php echo $dateOfBooking ?></td>
-                    <td><?php echo $roomName ?></td>
-                    <td><?php echo $period ?></td>
-                    <td >
- 						<?php echo "$firstName $lastName"; ?>
-                    </td>
-                    <td>
-                    	<button onclick="deleteAjax('<?php echo $dateOfBooking ?>', '<?php echo $classID ?>', '<?php echo $period ?>' )" class="btn btn-danger">Cancel</button>
-                    </td>
-                </tr>
-                  </tr>
-                </tbody>
-              <?php } ?>
-			</table> 
-		</section>
+		              $result = mysqli_query($server, $query);
+		              while($row = mysqli_fetch_array($result))
+		              {   //Creates a loop to loop through results
+
+
+		                $dateOfBooking = $row['dateOfBooking'];
+		                $period = $row['period'];
+		                $classID = $row['classID'];
+		                $teacherID = $row['teacherEmail'];
+
+		                $sql1 = "SELECT * FROM classroom WHERE classroom.classID = $classID";
+		                $resultClassroom =  mysqli_query($server, $sql1);
+		                $resultName = mysqli_fetch_array($resultClassroom);
+		                $roomName = $resultName['roomName'];
+
+		                $sql2 = "SELECT * FROM teacher WHERE teacher.email = '$teacherID'";
+		                $resultTeacher = mysqli_query($server, $sql2);
+		                $teacher = mysqli_fetch_array($resultTeacher);
+		                $firstName = $teacher['first_name'];
+		                $lastName = $teacher['last_name'];
+
+		                ?>
+
+		                <tbody>
+		                  <tr id="delete<?php echo $dateOfBooking; echo "$classID"; echo "$period"; ?>">
+		                    <td><?php echo $dateOfBooking ?></td>
+		                    <td><?php echo $roomName ?></td>
+		                    <td><?php echo $period ?></td>
+		                    <td >
+		 						<?php echo "$firstName $lastName"; ?>
+		                    </td>
+		                    <td>
+		                    	<button onclick="deleteAjax('<?php echo $dateOfBooking ?>', '<?php echo $classID ?>', '<?php echo $period ?>' )" class="btn btn-danger">Cancel</button>
+		                    </td>
+		                </tr>
+		                  </tr>
+		                </tbody>
+		              <?php } ?>
+					</table>
+				</div>
+			</div>
+	</section>
 
 
 		<script src="../../javascript/jquery.min.js"></script>
--- assets/php/viewBooking.php
+++ assets/php/viewBooking.php
@@ -46,55 +46,61 @@ $teacherEmail = $_SESSION['email'];
 
 			</div>
 
+			<div class="row">
+				<div class="calendar col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<input id="date" type="date" value="yyyy-mm-dd">
+				</div>
+				<div class="col">
+					<table id="bookings">
+						<thead>
+							<th>Date</th>
+							<th>Room Number</th>
+							<th>Period</th>
+							<th> Teacher</th>
+						</thead>
+						<?php
 
-			<table id="bookings">
-				<thead>
-					<th>Date</th>
-					<th>Room Number</th>
-					<th>Period</th>
-					<th> Teacher</th>
-				</thead>
-				<?php
- 
-              $currentTeacherID = $_SESSION['email'];
-              $query = "SELECT * FROM booking" ;
- 
-              $result = mysqli_query($server, $query);
-              while($row = mysqli_fetch_array($result))
-              {   //Creates a loop to loop through results
- 
- 
-                $dateOfBooking = $row['dateOfBooking'];
-                $period = $row['period'];
-                $classID = $row['classID'];
-                $teacherID = $row['teacherEmail'];
- 
-                $sql1 = "SELECT * FROM classroom WHERE classroom.classID = $classID";
-                $resultClassroom =  mysqli_query($server, $sql1);
-                $resultName = mysqli_fetch_array($resultClassroom);
-                $roomName = $resultName['roomName'];
+		              $currentTeacherID = $_SESSION['email'];
+		              $query = "SELECT * FROM booking" ;
 
-                $sql2 = "SELECT * FROM teacher WHERE teacher.email = '$teacherID'";
-                $resultTeacher = mysqli_query($server, $sql2);
-                $teacher = mysqli_fetch_array($resultTeacher);
-                $firstName = $teacher['first_name'];
-                $lastName = $teacher['last_name'];
- 
-                ?>
- 
-                <tbody>
-                  <tr id="delete<?php echo $dateOfBooking; echo "$classID"; echo "$period"; ?>">
-                    <td><?php echo $dateOfBooking ?></td>
-                    <td><?php echo $roomName ?></td>
-                    <td><?php echo $period ?></td>
-                    <td >
- 						<?php echo "$firstName $lastName"; ?>
-                    </td>
-                  </tr>
-                </tbody>
-              <?php } ?>
-			</table> 
-		</section>
+		              $result = mysqli_query($server, $query);
+		              while($row = mysqli_fetch_array($result))
+		              {   //Creates a loop to loop through results
+
+
+		                $dateOfBooking = $row['dateOfBooking'];
+		                $period = $row['period'];
+		                $classID = $row['classID'];
+		                $teacherID = $row['teacherEmail'];
+
+		                $sql1 = "SELECT * FROM classroom WHERE classroom.classID = $classID";
+		                $resultClassroom =  mysqli_query($server, $sql1);
+		                $resultName = mysqli_fetch_array($resultClassroom);
+		                $roomName = $resultName['roomName'];
+
+		                $sql2 = "SELECT * FROM teacher WHERE teacher.email = '$teacherID'";
+		                $resultTeacher = mysqli_query($server, $sql2);
+		                $teacher = mysqli_fetch_array($resultTeacher);
+		                $firstName = $teacher['first_name'];
+		                $lastName = $teacher['last_name'];
+
+		                ?>
+
+		                <tbody>
+		                  <tr id="delete<?php echo $dateOfBooking; echo "$classID"; echo "$period"; ?>">
+		                    <td><?php echo $dateOfBooking ?></td>
+		                    <td><?php echo $roomName ?></td>
+		                    <td><?php echo $period ?></td>
+		                    <td >
+		 						<?php echo "$firstName $lastName"; ?>
+		                    </td>
+		                  </tr>
+		                </tbody>
+		              <?php } ?>
+					</table>
+				</div>
+			</div>
+	</section>
 
 
 		<script src="../javascript/jquery.min.js"></script>
