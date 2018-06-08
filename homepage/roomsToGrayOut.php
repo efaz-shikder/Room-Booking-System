807cfd088d877b5c95c04143f13049b1b451985d
Unknown <efaz.a.shikder@gmail.com>
2018-06-08 00:32:53 -0400

    move $row

--- homepage/roomsToGrayOut.php
+++ homepage/roomsToGrayOut.php
@@ -28,12 +28,12 @@
 	{
 		$sql = "SELECT * from booking WHERE booking.classID = '$classID[$i]' AND booking.dateOfBooking ='$date' AND booking.period='$period'";
 		$bookingResult = mysqli_query($server, $sql);
-		$row = mysqli_fetch_array($bookingResult);
 
 		if (mysqli_num_rows($bookingResult) > 0)
 		{
 			// booking exists
 			// get the teacherID of the one result
+			$row = mysqli_fetch_array($bookingResult);
 			$teacherEmail = $row['teacherEmail'];
 			array_push($bookedRoomIDs, array($teacherEmail, $classID[$i]));
 			// $bookedRoomIDs[] = array($teacherEmail, $data[i]);
