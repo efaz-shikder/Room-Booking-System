fb107320c5a0067f51c9b3fe35e35c32b616e175
Unknown <efaz.a.shikder@gmail.com>
2018-06-08 01:10:55 -0400

    trying different stuff

--- homepage/roomsToGrayOut.php
+++ homepage/roomsToGrayOut.php
@@ -6,10 +6,6 @@
 	$date = $_POST['date'];
 	$period = $_POST['period'];
 	$currentTeacherID = $_SESSION['email'];
-
-	echo $hallway;
-	echo $date;
-	echo $period;
 	
 	$hallway = str_replace('"', "", $hallway);
 
@@ -26,19 +22,20 @@
 	$bookedRoomIDs = array();
 	for ($i = 0; $i < count($classID); $i++)
 	{
-		$sql = "SELECT * from booking WHERE booking.classID = '$classID[$i]' AND booking.dateOfBooking ='$date' AND booking.period='$period'";
+		$sql = "SELECT * from booking WHERE booking.classID='$classID[$i]' AND booking.dateOfBooking='$date' AND booking.period='$period'";
 		$bookingResult = mysqli_query($server, $sql);
 
-		if (mysqli_num_rows($bookingResult) > 0)
+		//if (mysqli_num_rows($bookingResult) > 0)
+		if($row = mysqli_fetch_array($bookingResult))
 		{
 			// booking exists
 			// get the teacherID of the one result
-			$row = mysqli_fetch_array($bookingResult);
+			// $row = mysqli_fetch_array($bookingResult);
 			$teacherEmail = $row['teacherEmail'];
 			array_push($bookedRoomIDs, array($teacherEmail, $classID[$i]));
 			// $bookedRoomIDs[] = array($teacherEmail, $data[i]);
 		}
 	}
 
-	$_SESSION['bookedRoomIDs'] = $bookedRoomIDs;
+	$_SESSION['bookedRoomIDs'] = [["efaz.shikder", 37], ["LUL", 38]];
 ?>
