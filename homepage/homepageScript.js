85ff5b40ed852dfb94fe52467d8e700480804f2e
Shaibal <shaibalmuhtadee@gmail.com>
2018-06-19 01:02:26 -0400

    fixed calendar bug

--- homepage/homepageScript.js
+++ homepage/homepageScript.js
@@ -280,13 +280,15 @@ createDay: function (num, day, year) {
 			month1 = noSchoolArray[n].substring(5,7) - 1;
 			day1 = noSchoolArray[n].substring(8,10);
 			
-			if ((this.date.getTime() <= this.todaysDate.getTime() - 1) 
+			if ( (this.date.getTime() <= this.todaysDate.getTime() - 1) 
 				|| (this.date.getMonth() === 6) 
 				|| (this.date.getMonth() === 7)
-				|| (weekend == "Sat") || (weekend == "Sun") 
-				|| ((this.date.getFullYear() == year1 && this.date.getMonth() == month1) && (this.date.getDate() == day1) ) ) 
+				|| (weekend == "Sat") 
+				|| (weekend == "Sun")
+				|| ( (this.date.getFullYear() == year1 && this.date.getMonth() == month1) && (this.date.getDate() == day1) ) ) 
 			{
-				newDay.classList.add('cal__date--disabled')
+				newDay.classList.add('cal__date--disabled');
+				newDay.classList.add('disabled5');
 			} 
 			else 
 			{
@@ -295,22 +297,6 @@ createDay: function (num, day, year) {
 			}
 		}
 	}
-	else
-	{
-		if ((this.date.getTime() <= this.todaysDate.getTime() - 1) 
-			|| (this.date.getMonth() === 6) 
-			|| (this.date.getMonth() === 7)
-			|| (weekend == "Sat") || (weekend == "Sun"))
-		{
-			newDay.classList.add('cal__date--disabled')
-		} 
-		else 
-		{
-			newDay.classList.add('cal__date--active')
-			newDay.setAttribute('data-calendar-status', 'active')
-		}		
-	}
-
 	newDay.appendChild(dateEl)
 	this.month.appendChild(newDay)
 },
