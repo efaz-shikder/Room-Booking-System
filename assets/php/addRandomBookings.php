f78aeb3eed5c89eab0891221d777a8e19fca731f
Ashfi <ashfipathan@gmail.com>
2018-06-19 02:18:36 -0400

    random bookings

--- assets/php/addRandomBookings.php
+++ assets/php/addRandomBookings.php
@@ -2,18 +2,18 @@
 session_start();
 include_once("connect.php");
 
-$periodArray = ['A', 'B', 'C', 'D'];
-$teacherArray = ['ashfipathan@gmail.com', 'shaibalmuhtadee@gmail.com', 'efaz.es@gmail.com'];
+$periodArray = ['A', 'B', 'C', 'D', 'AS', 'L', 'BS'];
+$teacherArray = ['ashfipathan@gmail.com', 'shaibalmuhtadee@gmail.com', 'efaz.es@gmail.com', 'josh.prested@gmail.com', 'orson.sampani@gmail.com', 'amyqin501@gmail.com'];
 
 for ($i = 0; $i < 101; $i++)
 {
 	$randomClassroom = rand(1, 70);
 
-	$randomTeacher = $teacherArray[rand(0, 2)];
+	$randomTeacher = $teacherArray[rand(0, 5)];
 
-	$randomPeriod = $periodArray[(rand(0, 3))]; 
+	$randomPeriod = $periodArray[(rand(0, 6))]; 
 	
-	$randomDate = "2018-06-" . rand(5, 28); 
+	$randomDate = "2018-06-" . rand(5, 19); 
 	
 	$sql = "INSERT INTO booking (`teacherEmail`, `classID`, `dateOfBooking`, `period`) VALUES ('$randomTeacher', '$randomClassroom', '$randomDate', '$randomPeriod')"; 
 	$result = mysqli_query($server, $sql);
